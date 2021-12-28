ae89cb24daaeea06cc1615c658225e634df06621
soapmactavish23 <ricknogueira1231@gmail.com>
2021-12-27 21:03:26 -0300

    Criando a Tela Base

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-void main() async {
+import 'package:loja_virtual/screens/base/base_screen.dart';
 
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
   runApp(const MyApp());
-
-  FirebaseFirestore.instance.collection("teste").add({"teste": "teste"});
-
 }
 
 class MyApp extends StatelessWidget {
@@ -19,11 +17,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Loja do Henrick',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: BaseScreen(),
     );
   }
 }
