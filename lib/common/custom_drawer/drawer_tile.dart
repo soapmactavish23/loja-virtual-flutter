0dfda25b515fe3e9f6e9735667490a7019a0452c
soapmactavish23 <ricknogueira1231@gmail.com>
2021-12-27 21:39:21 -0300

    Navegando via Provider

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
 class DrawerTile extends StatelessWidget {
   
   DrawerTile({required this.iconData, required this.title, required this.page});
@@ -10,8 +11,12 @@ class DrawerTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final int curPage = context.watch<PageManager>().page;
+
     return InkWell(
       onTap: () {
+        context.read<PageManager>().setPage(page);
         debugPrint("Toquei");
       },
       child: SizedBox(
@@ -23,7 +28,7 @@ class DrawerTile extends StatelessWidget {
               child: Icon(
                 this.iconData,
                 size: 32,
-                color: Colors.grey[700],
+                color: curPage == page ? Colors.red : Colors.grey[700],
               ),
             ),
             Text(
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: [
-        Scaffold(drawer: CustomDrawer(), appBar: AppBar(title: Text("Home"))),
-        Container(
-          color: Colors.red,
-        ),
-        Container(color: Colors.yellow),
-        Container(color: Colors.green),
-      ],
+
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Scaffold(drawer: CustomDrawer(), appBar: AppBar(title: Text("Home"))),
+          Container(
+            color: Colors.red,
+          ),
+          Container(color: Colors.yellow),
+          Container(color: Colors.green),
+        ],
+      ),
     );
   }
 }
