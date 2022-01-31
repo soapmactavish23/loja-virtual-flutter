9538ba10e2e6c50198ea9633a25f558ec196ab3b
HKprogram <ricknogueira1231@gmail.com>
2022-01-31 15:50:46 -0300

    Validando o Campo de Imagens

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -14,6 +14,13 @@ class ImagesForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return FormField<List<dynamic>>(
       initialValue: List.from(product.images),
+      validator: (images) {
+        if (images!.isEmpty) {
+          return 'Insira ao menos uma imagem';
+        } else {
+          return null;
+        }
+      },
       builder: (state) {
         void onImageSelected(File file) {
           state.value!.add(file);
@@ -21,66 +28,85 @@ class ImagesForm extends StatelessWidget {
           Navigator.pop(context);
         }
 
-        return AspectRatio(
-          aspectRatio: 1,
-          child: CarouselSlider(
-            options: CarouselOptions(
-              height: 400.0,
-              viewportFraction: 1.0,
-              autoPlay: false,
+        return Column(
+          children: [
+            AspectRatio(
               aspectRatio: 1,
-            ),
-            items: state.value!.map<Widget>(
-              (image) {
-                return Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    if (image is String)
-                      Image.network(
-                        image,
-                        fit: BoxFit.cover,
-                      )
-                    else
-                      Image.file(
-                        image as File,
-                        fit: BoxFit.cover,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 400.0,
+                  viewportFraction: 1.0,
+                  autoPlay: false,
+                  aspectRatio: 1,
+                ),
+                items: state.value!.map<Widget>(
+                  (image) {
+                    return Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        if (image is String)
+                          Image.network(
+                            image,
+                            fit: BoxFit.cover,
+                          )
+                        else
+                          Image.file(
+                            image as File,
+                            fit: BoxFit.cover,
+                          ),
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            icon: const Icon(Icons.remove),
+                            color: Colors.red,
+                            onPressed: () {
+                              state.value!.remove(image);
+                              state.didChange(state.value);
+                            },
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ).toList()
+                  ..add(
+                    SizedBox(
+                      width: 400,
+                      child: Material(
+                        color: Colors.grey[100],
+                        child: IconButton(
+                          icon: const Icon(Icons.add_a_photo),
+                          color: Theme.of(context).primaryColor,
+                          iconSize: 50,
+                          onPressed: () {
+                            showModalBottomSheet(
+                                context: context,
+                                builder: (_) => ImageSourceSheet(
+                                      onImageSelected: onImageSelected,
+                                    ));
+                          },
+                        ),
                       ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: IconButton(
-                        icon: const Icon(Icons.remove),
-                        color: Colors.red,
-                        onPressed: () {
-                          state.value!.remove(image);
-                          state.didChange(state.value);
-                        },
-                      ),
-                    )
-                  ],
-                );
-              },
-            ).toList()
-              ..add(
-                SizedBox(
-                  width: 400,
-                  child: Material(
-                    color: Colors.grey[100],
-                    child: IconButton(
-                      icon: const Icon(Icons.add_a_photo),
-                      color: Theme.of(context).primaryColor,
-                      iconSize: 50,
-                      onPressed: () {
-                        showModalBottomSheet(
-                            context: context,
-                            builder: (_) => ImageSourceSheet(
-                                  onImageSelected: onImageSelected,
-                                ));
-                      },
                     ),
+                  ),
+              ),
+            ),
+            if (state.hasError)
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 16,
+                  left: 16,
+                ),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  state.errorText!,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 12,
                   ),
                 ),
               ),
-          ),
+          ],
         );
       },
     );
--- lib/screens/edit_product/edit_product_screen.dart
+++ lib/screens/edit_product/edit_product_screen.dart
@@ -3,9 +3,11 @@ import 'package:loja_virtual/models/product.dart';
 import 'package:loja_virtual/screens/edit_product/components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
-  EditProductScreen(this.product);
+  EditProductScreen(this.product, {Key? key}) : super(key: key);
 
   final Product product;
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +16,24 @@ class EditProductScreen extends StatelessWidget {
           title: const Text('Editar Anúncio'),
           centerTitle: true,
         ),
-        body: ListView(
-          children: [
-            ImagesForm(
-              product: product,
-            ),
-          ],
+        backgroundColor: Colors.white,
+        body: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              ImagesForm(
+                product: product,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    debugPrint('VALIDO');
+                  }
+                },
+                child: const Text('Salvar'),
+              ),
+            ],
+          ),
         ));
   }
 }
