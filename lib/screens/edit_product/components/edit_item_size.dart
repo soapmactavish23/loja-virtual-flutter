f6fd80714c0b6f8ff26742f72bb519b5f603da00
HKprogram <ricknogueira1231@gmail.com>
2022-02-10 18:25:53 -0300

    Adicionando/Removendo um Tamanho

--- lib/screens/edit_product/components/edit_item_size.dart
+++ lib/screens/edit_product/components/edit_item_size.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_icon_buttom.dart';
 import 'package:loja_virtual/models/item_size.dart';
 
 class EditItemSize extends StatelessWidget {
   final ItemSize size;
-
-  const EditItemSize({Key? key, required this.size}) : super(key: key);
+  final VoidCallback onRemove;
+  const EditItemSize({Key? key, required this.size, required this.onRemove})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Expanded(
+          flex: 30,
           child: TextFormField(
             initialValue: size.name,
             decoration: const InputDecoration(
@@ -23,6 +26,7 @@ class EditItemSize extends StatelessWidget {
           width: 5,
         ),
         Expanded(
+          flex: 30,
           child: TextFormField(
             initialValue: size.stock.toString(),
             decoration: const InputDecoration(
@@ -36,14 +40,29 @@ class EditItemSize extends StatelessWidget {
           width: 5,
         ),
         Expanded(
+          flex: 40,
           child: TextFormField(
             initialValue: size.price.toStringAsFixed(2),
             decoration: const InputDecoration(
               labelText: 'Preço',
               isDense: true,
+              prefixText: 'R\$',
             ),
             keyboardType: TextInputType.number,
           ),
+        ),
+        CustomIconButton(
+          iconData: Icons.remove,
+          color: Colors.red,
+          onTap: onRemove,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_up,
+          color: Colors.black,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_down,
+          color: Colors.black,
         ),
       ],
     );
--- lib/screens/edit_product/components/sizes_form.dart
+++ lib/screens/edit_product/components/sizes_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_icon_buttom.dart';
 import 'package:loja_virtual/models/item_size.dart';
 import 'package:loja_virtual/models/product.dart';
 import 'package:loja_virtual/screens/edit_product/components/edit_item_size.dart';
@@ -10,15 +11,50 @@ class SizesForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FormField<List<ItemSize>>(
-      initialValue: product.sizes,
-      builder: (state) {
-        return Column(
-          children: state.value!.map((size) {
-            return EditItemSize(size: size);
-          }).toList(),
-        );
-      },
+    return Column(
+      children: [
+        FormField<List<ItemSize>>(
+          initialValue: List.from(product.sizes),
+          builder: (state) {
+            return Column(
+              children: [
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Text(
+                        'Tamanhos',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    CustomIconButton(
+                      iconData: Icons.add,
+                      color: Colors.black,
+                      onTap: () {
+                        state.value!.add(ItemSize());
+                        state.didChange(state.value);
+                      },
+                    )
+                  ],
+                ),
+                Column(
+                  children: state.value!.map((size) {
+                    return EditItemSize(
+                      size: size,
+                      onRemove: () {
+                        state.value!.remove(size);
+                        state.didChange(state.value);
+                      },
+                    );
+                  }).toList(),
+                )
+              ],
+            );
+          },
+        ),
+      ],
     );
   }
 }
