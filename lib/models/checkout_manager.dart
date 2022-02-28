fc417979a231543ca0af889dc4d7958ac6ec469a
soapmactavish23 <ricknogueira1231@gmail.com>
2022-02-28 17:03:21 -0300

    Verificando Estoque e Decrementando

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -1,10 +1,10 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:loja_virtual/models/cart_manager.dart';
+import 'package:loja_virtual/models/cart_product.dart';
+import 'package:loja_virtual/models/product.dart';
 
 class CheckoutManager extends ChangeNotifier {
-
   CartManager? cartManager;
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -13,8 +13,12 @@ class CheckoutManager extends ChangeNotifier {
     this.cartManager = cartManager;
   }
 
-  void checkout() {
-    _decrementStock();
+  Future<void> checkout() async {
+    try {
+      await _decrementStock();
+    } catch (e) {
+      debugPrint(e.toString());
+    }
 
     _getOrderId().then((value) => print(value));
   }
@@ -23,20 +27,58 @@ class CheckoutManager extends ChangeNotifier {
     final ref = firestore.doc('aux/ordercounter');
     try {
       final result = await firestore.runTransaction((tx) async {
-      final doc = await tx.get(firestore.doc('aux/ordercounter'));
-      final orderId = doc['current'] as int;
-      tx.update(ref, {'current': orderId + 1});
-      return {'orderId': orderId};
-    }); 
-    return result['orderId'] as int;
-    } catch(e) {
+        final doc = await tx.get(firestore.doc('aux/ordercounter'));
+        final orderId = doc['current'] as int;
+        tx.update(ref, {'current': orderId + 1});
+        return {'orderId': orderId};
+      });
+      return result['orderId'] as int;
+    } catch (e) {
       debugPrint(e.toString());
       return Future.error('Falha ao gerar número do pedido');
     }
   }
 
-  void _decrementStock() {
+  Future<void> _decrementStock() async {
+    return await firestore.runTransaction((tx) async {
+      final List<Product> productsToUpdate = [];
+      final List<Product> productsWithoutStock = [];
 
+      for (final cartProduct in cartManager!.items) {
+        Product product;
+
+        if (productsToUpdate.any((p) => p.id == cartProduct.productId)) {
+          product =
+              productsToUpdate.firstWhere((p) => p.id == cartProduct.productId);
+        } else {
+          final doc = await tx.get(
+            firestore.doc('products/${cartProduct.productId}'),
+          );
+          product = Product.fromDocument(doc);
+        }
+
+        cartProduct.product = product;
+
+        final size = product.findSize(cartProduct.size);
+        if (size!.stock! - cartProduct.quantity < 0) {
+          productsWithoutStock.add(product);
+        } else {
+          int res = size.stock!;
+          res -= cartProduct.quantity;
+          size.stock = res;
+          productsToUpdate.add(product);
+        }
+      }
+
+      if (productsWithoutStock.isNotEmpty) {
+        return Future.error(
+            '${productsWithoutStock.length} produtos sem estoque');
+      }
+
+      for (final product in productsToUpdate) {
+        tx.update(firestore.doc('products/${product.id}'),
+            {'sizes': product.exportSizeList()});
+      }
+    });
   }
-  
 }
