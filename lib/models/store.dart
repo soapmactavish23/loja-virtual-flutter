9f53f373634337522f3f2becb0d09eb74ce9ae9e
HKprogram <ricknogueira1231@gmail.com>
2022-03-14 11:08:06 -0300

    Criando o Objeto da Loja

---
+++ lib/models/store.dart
@@ -0,0 +1,69 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/address.dart';
+
+class Store {
+  String name = "";
+  String image = "";
+  String phone = "";
+  Address address;
+  Map<String, dynamic> opening;
+  Store({
+    required this.name,
+    required this.image,
+    required this.phone,
+    required this.address,
+    required this.opening,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'image': image,
+      'phone': phone,
+      'address': address.toMap(),
+      'opening': opening,
+    };
+  }
+
+  factory Store.fromMap(Map<String, dynamic> map) {
+    return Store(
+      name: map['name'] ?? '',
+      image: map['image'] ?? '',
+      phone: map['phone'] ?? '',
+      address: Address.fromMap(map['address']),
+      opening: (map['opening'] as Map<String, dynamic>).map((key, value) {
+        final timesString = value as String;
+
+        if (timesString != "" && timesString.isNotEmpty) {
+          final splitted = timesString.split(RegExp(r"[:-]"));
+          return MapEntry(
+            key,
+            {
+              "from": TimeOfDay(
+                hour: int.parse(splitted[0]),
+                minute: int.parse(splitted[1]),
+              ),
+              "to": TimeOfDay(
+                hour: int.parse(splitted[2]),
+                minute: int.parse(splitted[3]),
+              ),
+            },
+          );
+        } else {
+          return MapEntry(key, null);
+        }
+      }),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Store.fromJson(String source) => Store.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Store(name: $name, image: $image, phone: $phone, address: $address, opening: $opening)';
+  }
+}
--- lib/models/stores_manager.dart
+++ lib/models/stores_manager.dart
@@ -1,23 +1,22 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:loja_virtual/models/store.dart';
 
 class StoresManager extends ChangeNotifier {
-
   StoresManager() {
     _loadStoreList();
   }
 
+  List<Store> stores = [];
+
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   Future<void> _loadStoreList() async {
-
     final snapshot = await firestore.collection('stores').get();
 
-    print(snapshot.docs.first.data());
+    stores = snapshot.docs.map((store) => Store.fromMap(store.data())).toList();
+    print(stores);
 
     notifyListeners();
-
   }
-  
 }
