7240c11a72dd48fb9426b9276175898f88a3f513
HKprogram <ricknogueira1231@gmail.com>
2022-03-23 16:51:35 -0300

    Configurando o Package da Cielo

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,34 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+import { CieloConstructor, Cielo } from 'cielo';
 
 admin.initializeApp();
+
+const merchantId = functions.config().cielo.merchantId;
+const merchantKey = functions.config().cielo.merchantKey;
+
+
+const cieloParams = {
+    merchantId: merchantId,
+    merchantKey: merchantKey,
+    sandbox: true,
+    debug: true
+}
+
+const cielo = new Cielo(cieloParams);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onCall((request, response) => {
-  functions.logger.info({"request": request}, {structuredData: true});
-  return {data: "Hello from loja virtual"};
+  functions.logger.info({ "request": request }, { structuredData: true });
+  return { data: "Hello from loja virtual" };
 });
 
 exports.getUserData = functions.https.onCall((data, context) => {
-  functions.logger.info("Get user data", {structuredData: true});
+  functions.logger.info("Get user data", { structuredData: true });
   if (!context.auth) {
-    return {data: "Nenhum usuário logado!"};
+    return { data: "Nenhum usuário logado!" };
   }
   return admin.firestore().collection("users").doc(context.auth.uid).get().then((snapshot) => snapshot.data());
 });
